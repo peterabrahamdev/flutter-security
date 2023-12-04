35bd29e1a5cf84c33371eb39dc2986141a291301
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-12-04 22:37:00 +0100

    feat(authentication): add biometric authentication

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger();
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +13,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future<void> authenticate() async {
+    LocalAuthentication auth = LocalAuthentication();
+    try {
+      final bool didAuthenticate = await auth.authenticate(
+        localizedReason: 'Authenticate yourself to continue',
+        options: const AuthenticationOptions(
+          biometricOnly: true,
+        ),
+      );
+    } on PlatformException catch (e) {
+      logger.e('Error during authentication: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Flutter Security')),
-      body: const Center(child: Text('Flutter Security')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Press to authenticate'),
+          onPressed: () => authenticate(),
+        ),
+      ),
     );
   }
 }

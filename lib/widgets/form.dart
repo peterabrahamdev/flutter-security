04b8a7dbfef015f0dc0af458006788d44a9a6731
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-12-05 14:53:30 +0100

    feat(form): add validators to TextFormFields

--- lib/widgets/form.dart
+++ lib/widgets/form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
@@ -9,6 +10,7 @@ class AuthForm extends StatefulWidget {
 
 class _AuthFormState extends State<AuthForm> {
   final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -17,13 +19,27 @@ class _AuthFormState extends State<AuthForm> {
         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
         child: Column(children: [
           TextFormField(
-            decoration: const InputDecoration(labelText: 'Username'),
+            decoration: const InputDecoration(
+              labelText: 'Username',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a username';
+              }
+              return null;
+            },
           ),
           TextFormField(
             decoration: const InputDecoration(
               labelText: 'Password',
             ),
             obscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a password';
+              }
+              return null;
+            },
           ),
         ]),
       ),
